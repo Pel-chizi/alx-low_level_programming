800be6aaec8ffa7d36f4e2abc4c26c5820a2f753
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-17 13:55:34 -0800

    putting a line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The main function that prints output using puts function
+ * Return: 0 (means compilation successful)
+ */
+int main(void)
+{
+puts("\ "Programming is like building a multilingual puzzle");
+return (0);
+}
