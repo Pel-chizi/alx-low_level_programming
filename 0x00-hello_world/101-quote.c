003e0d532016e22733d8348d15a65b62138188a6
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-18 13:00:46 -0800

    to std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - printing a line followed by a new line to the std error
@@ -8,4 +7,3 @@ int main(void)
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-
