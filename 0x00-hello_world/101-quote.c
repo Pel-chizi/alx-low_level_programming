793a0bfe56689dde44a20807ba89868e624916ac
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-18 12:57:01 -0800

    printing a line to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - printing a line to the std error
+ * main - printing a line followed by a new line to the std error
  * Return: 1 (for this program)
  */
 int main(void)
-{
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
 
