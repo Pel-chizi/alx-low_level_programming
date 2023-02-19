ce298da3463b06cbdf5cdeb960734e128f6fbfb3
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-19 10:50:07 -0800

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints numbers zero to nine
+ * Return: 0(correct)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
