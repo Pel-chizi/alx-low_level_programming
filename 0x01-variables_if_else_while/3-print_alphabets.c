26901f9ec9481fc9c65f7404b9b6c8c825ab5ae1
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-19 10:11:38 -0800

    print lower n upper alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing alphabets in lower n upper cases
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	putchar(l);
+
+	for (l = 'A'; l <= 'Z'; l++)
+	putchar(l);
+	return (0);
+}
