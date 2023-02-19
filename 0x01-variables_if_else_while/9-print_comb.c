0742dbac7e5b7b55dbf2ebc331bd08073a51397b
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-19 11:36:57 -0800

    single digits with commas n space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - digits with commas and space betwixt them
+ * Return: 0(correct)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
