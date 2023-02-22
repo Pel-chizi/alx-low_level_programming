bedefabf368ff1e9e87322d08530085c69bcaad0
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-22 07:24:02 -0800

    sum of multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - sum of all multiples of 3 and 5 excluding 1024
+ * Return: 0 correct
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	while (i < 1024)
+	{
+		if  ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z = z + i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
