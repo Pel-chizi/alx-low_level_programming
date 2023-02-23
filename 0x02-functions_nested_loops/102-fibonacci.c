387d43b3ce818a48ca7cc3304c2b077511083493
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-22 16:35:48 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - printing first 50 fibonacci numbers
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i = 0;
+	int x = 1;
+	int y = 2;
+
+	while (i < 50)
+	{
+	if (i == 0)
+	printf("%d", x);
+	else if (i == 1)
+	printf(", %d", y);
+	else
+	{
+	y = y + x;
+	x = y - x;
+	printf(", %d", y);
+	}
+	++i;
+	}
+	print("\n");
+	return (0);
+}
