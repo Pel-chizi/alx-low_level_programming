188cc837bbf38c1bab3b12357054ac773cd9c988
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-22 18:23:14 -0800

    first 98 bibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,16 +6,38 @@
 int main(void)
 {
 	int i;
-	long t1 = 1, t2 = 2;
-	long nextTerm = t1 + t2;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_part1, fib1_part2, fib2_part1, fib2_part2;
+	unsigned long part1, part2;
 
-	printf(" %ld, %ld, ", t1, t2);
-	for (i = 3; i < 99; ++i)
+	for (i = 0; i < 92; i++)
 	{
-		printf("%ld, ", nextTerm);
-		t1 = t2;
-		t2 = nextTerm;
-		nextTerm = t1 + t2;
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
+		fib1 = fib2;
+		fib2 = sum;
 	}
+	fib1_part1 = fib1 / 10000000000;
+	fib2_part1 = fib2 / 10000000000;
+	fib1_part2 = fib1 % 10000000000;
+	fib2_part2 = fib2 % 10000000000;
+	for (i = 93; i < 99; i++)
+	{
+		part1 = fib1_part1 + fib2_part1;
+		part2 = fib1_part2 + fib2_part2;
+		if (fib1_part2 + fib2_part2 > 9999999999)
+		{
+			part1 += 1;
+			part2 %= 10000000000;
+		}
+		printf("%lu%lu", part1, part2);
+		if (i != 98)
+			printf(", ");
+		fib1_part1 = fib2_part1;
+		fib1_part2 = fib2_part2;
+		fib2_part1 = part1;
+		fib2_part2 = part2;
+	}
+	printf("\n");
 	return (0);
 }
