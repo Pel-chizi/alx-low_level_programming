6f0d4d9fce967e269617745372a128bd320212cd
Pel-chizi <pelumiadediran11@gmail.com>
2023-02-27 10:40:57 -0800

    copying a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - copying a string
+ * @dest: the destination
+ * @src: the source
+ * Return: the pointer to the destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
