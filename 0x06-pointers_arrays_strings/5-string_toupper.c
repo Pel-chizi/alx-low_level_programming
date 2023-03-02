f7b83f18df3ef36d3e0ad9e1ee5398f97e742033
Pel-chizi <pelumiadediran11@gmail.com>
2023-03-02 08:01:44 -0800

    converts from lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes from lowercase to upper
+ * @c: pointer, to the character
+ * Return: c
+ */
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - 32;
+		}
+		i++;
+	}
+	return (c);
+}
