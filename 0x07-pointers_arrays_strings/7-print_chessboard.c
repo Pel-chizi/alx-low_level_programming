fa2682c08f6f9b5315fb65b303d558453d7b094f
Pel-chizi <pelumiadediran11@gmail.com>
2023-03-07 08:43:31 -0800

    printing the chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: the array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
